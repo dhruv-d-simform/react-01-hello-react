d337b57e53ff44e838fc0ddf7c97d6bc7eb51e8b
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-04-24 17:08:52 +0530

    feature: Add the name, email, github id information in App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import "./index.css";
+
 function App() {
   return (
-    <div>Hello, World</div>
+    <div className="container">
+      <p>Hello, My name is Dhruv Dhaduk.</p>
+      <p>My email is : <a href="mailto:dhruv.dhaduk@simformsolutions.com">dhruv.dhaduk@simformsolutions.com</a></p>
+      <p>My GitHub account : <a href="https://github.com/dhruvdhaduk-simform">https://github.com/dhruvdhaduk-simform</a></p>
+    </div>
   );
 }
 
